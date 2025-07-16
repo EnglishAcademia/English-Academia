1ff7ebd43196eaeef6d4c8ffa80a7f4db6102007
EnglishAcademia <omeothirteen@gmail.com>
2025-07-16 22:08:59 +0200

    Update scripts.js
    tweaking calendar

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const calendarToggleBtn = document.getElementById('calendar-toggle');
-const calendarSidebar = document.querySelector('.calendar-sidebar');
+  const calendarSidebar = document.querySelector('.calendar-sidebar');
 
-calendarToggleBtn.addEventListener('click', () => {
-  calendarSidebar.classList.toggle('visible');
+  calendarToggleBtn.addEventListener('click', () => {
+    calendarSidebar.classList.toggle('visible');
+  });
 });
+
 
