12b91a1e2455996b848b7e581393716d38cff621
Stanley Hurst <sdhurst@gmail.com>
2025-07-17 12:01:33 +0200

    change from google calendar to calenderly

--- scripts.js
+++ scripts.js
@@ -1,10 +1,2 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const calendarToggleBtn = document.getElementById('calendar-toggle');
-  const calendarSidebar = document.querySelector('.calendar-sidebar');
-
-  calendarToggleBtn.addEventListener('click', () => {
-    calendarSidebar.classList.toggle('visible');
-  });
-});
 
 
